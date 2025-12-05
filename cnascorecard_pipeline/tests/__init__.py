8518315b23140889b5044b909feec53080f4a24b
envrs <dr.lizadmf@gmail.com>
2025-12-05 15:59:22 +0000

    refactoring: CVEE Website

--- cnascorecard_pipeline/tests/__init__.py
+++
@@ -1,6 +0,0 @@
-"""
-Test package for CNA Scorecard Pipeline.
-
-This package contains unit tests for all pipeline modules to ensure
-correctness of data transformations and prevent regressions.
-"""
